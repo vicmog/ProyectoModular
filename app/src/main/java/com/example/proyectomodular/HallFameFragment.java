4d1471f3211a9576d99d59ffaa390934afb55bbb
josem <josem@DESKTOP-UGBH1NS>
2020-12-21 10:13:20 +0100

    Add bottom_navigation to HallFameFragment

--- app/src/main/java/com/example/proyectomodular/HallFameFragment.java
+++ app/src/main/java/com/example/proyectomodular/HallFameFragment.java
@@ -2,17 +2,54 @@ package com.example.proyectomodular;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HallFameFragment extends Fragment {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+
+    public HallFameFragment() {
+        // Required empty public constructor
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_hall_fame, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bottomNavigationView = view.findViewById(R.id.bottom_navigation);
+        navController = Navigation.findNavController(view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.players:navController.navigate(R.id.hallFameFragment);
+                        break;
+                    case R.id.jugarPartida:navController.navigate(R.id.juegoFragment);
+                        break;
+                    case R.id.adminArea:navController.navigate(R.id.adminFragment);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
