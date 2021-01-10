26ec08ff074ab7d35201ba4e7ea29bf875e39c9e
vicmog <62395770+vicmog@users.noreply.github.com>
2021-01-10 19:53:03 +0100

    commit shared preferences : se implementa el uso completo de las shared preferences para guardar la contraseña del administrador.Es decir solo podremos introducir la contraseña la primera vez que ejecurtemos la app.

--- app/src/main/java/com/example/proyectomodular/view/fragment/AdminPasswordInicioFragment.java
+++ app/src/main/java/com/example/proyectomodular/view/fragment/AdminPasswordInicioFragment.java
@@ -1,5 +1,6 @@
 package com.example.proyectomodular.view.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +25,10 @@ public class AdminPasswordInicioFragment extends Fragment {
 private TextInputEditText etPassword ,etRetryPassword;
 private TextInputLayout tilPassword,tilRetryPassword;
 private NavController navController;
+private SharedPreferences spPasswordAdmin;
+private final String KEY ="passwordAdmin";
+private SharedPreferences.Editor editor;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
@@ -31,6 +38,11 @@ private NavController navController;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        spPasswordAdmin = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        editor = spPasswordAdmin.edit();
+
+
         navController = Navigation.findNavController(getView());
         etPassword = getView().findViewById(R.id.tietPasswordAdmin);
         etRetryPassword = getView().findViewById(R.id.tietRetryPasswordAdmin);
@@ -40,11 +52,48 @@ private NavController navController;
         btAceptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navController.navigate(R.id.hallFameFragment);
+
+                if(compruebaClaves()){
+                    navController.navigate(R.id.hallFameFragment);
+
+                }
+
             }
         });
+        //POSIBLE METODO
+        String posibleClave = spPasswordAdmin.getString(KEY,"");
+        if(posibleClave.length() != 0 ){
+            navController.navigate(R.id.hallFameFragment);
+        }
 
 
 
+    }
+
+    private boolean compruebaClaves() {
+        String password = etPassword.getText().toString().trim();
+        String retryPassword = etRetryPassword.getText().toString().trim();
+
+        if(password.length() >= 4){
+            tilPassword.setErrorEnabled(false);
+                if(password.compareTo(retryPassword) == 0){
+                    tilRetryPassword.setErrorEnabled(false);
+
+
+                    editor.putString(KEY,retryPassword);
+                    editor.apply();
+                    editor.commit();
+
+                    return true;
+
+                }else{
+                    tilRetryPassword.setError("Las contraseñas no coinciden");
+                }
+
+        }else{
+            tilPassword.setError("La contraseña debe tener al menos 4 caracteres");
+        }
+
+        return false;
     }
 }
