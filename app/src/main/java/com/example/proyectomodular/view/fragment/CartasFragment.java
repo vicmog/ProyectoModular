fe35bfbe0a97a3be13876b3532b2e906375fc322
EAlba188 <73299712+EAlba188@users.noreply.github.com>
2021-01-30 20:17:23 +0100

    PRE FINAL PUSH

--- app/src/main/java/com/example/proyectomodular/view/fragment/CartasFragment.java
+++ app/src/main/java/com/example/proyectomodular/view/fragment/CartasFragment.java
@@ -1,12 +1,15 @@
 package com.example.proyectomodular.view.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -51,8 +54,33 @@ public class CartasFragment extends Fragment {
         newCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navController = Navigation.findNavController(view);
-                navController.navigate(R.id.newCardFragment);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("¿Qué deseas hacer?");
+                ViewModel vm = new ViewModelProvider((ViewModelStoreOwner) getContext()).get(ViewModel.class);
+
+                builder.setPositiveButton("Crear", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        navController = Navigation.findNavController(view);
+                        navController.navigate(R.id.newCardFragment);
+
+                    }
+                });
+                builder.setNegativeButton("Importar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.setNeutralButton("Cancelar", null);
+
+                AlertDialog alert = builder.create();
+                builder.show();
+
+
+
             }
         });
 
